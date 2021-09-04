e5ac05bd98b5b243ff2c4d1fa68f8d125f8683fd
Moritz Angermann <moritz.angermann@gmail.com>
2021-09-04 20:07:20 +0200

    add git

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-21.05.tar.gz") {} }:
 pkgs.mkShell {
   name = "Nix-Flakes-Test-Shell";
-  buildInputs = with pkgs; [ nixUnstable nixFlakes ];
+  buildInputs = with pkgs; [ nixUnstable nixFlakes git ];
   shellHook = ''
     echo "Welcome to the Nix Flakes Test Shell!";
   '';

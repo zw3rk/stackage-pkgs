4fb24d52ea5fe4a9bb3210e30090320e9e6a862c
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2021-09-22 21:42:11 +1200

    Switch darwin back on

--- flake.nix
+++ flake.nix
@@ -16,8 +16,8 @@
     flake-utils.lib.eachSystem [
       "x86_64-linux"
       "aarch64-linux"
-#      "x86_64-darwin"
-#      "aarch64-darwin"
+      "x86_64-darwin"
+      "aarch64-darwin"
     ] (system:
     let
       ifdLevel = 1;
